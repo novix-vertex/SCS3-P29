429960d5d55f46995c9114f530d2e11dcdc8129d
chirag jain <jchirag0610@gmail.com>
2026-06-12 06:26:29 +0530

    edit and delete done + element added using create element  method

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
+
 const input = document.querySelector("input");
 
 const addBtn = document.querySelector(".add-btn");
 const editBtn = document.querySelector(".edit-btn");
 const deleteBtn = document.querySelector(".delete-btn");
 
-const h1 = document.querySelector("h1");
-
 const todoList = document.querySelector("#todo-list");
 
-addBtn.addEventListener("click", () => {
+let taskId = null;
+let isEditing = false;
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     if (!input.value.trim()) {
         return;
     }
-    todoList.innerHTML += `<li class="todo-item">${input.value}
-                <div class="btns">
-                    <button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button>
-                </div>
-            </li>`;
+
+    let li = document.createElement("li");
+    li.setAttribute("class", "todo-item");
+    li.setAttribute("data-tid", "tid-" + Math.floor(Math.random() * 10000000000));
+
+    let h2 = document.createElement("h2");
+    h2.textContent = input.value;
+
+    let divBtns = document.createElement("div");
+    divBtns.setAttribute("class", "btns");
+
+    let editBtn = document.createElement("button");
+    editBtn.setAttribute("class", "edit-btn");
+    editBtn.textContent = "Edit";
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "delete-btn");
+    deleteBtn.textContent = "Delete";
+
+    if (isEditing) {
+        console.log("in isediting")
+        isEditing = false;
+        console.log(taskId);
+        let elem = document.querySelector(`[data-tid="${taskId}"] h2`);
+        elem.textContent = input.value;
+
+        input.value = "";
+        addBtn.textContent = "Add";
+        taskId = null;
+    } else {
+        console.log("in not isediting")
+        todoList.append(li);
+        li.append(h2, divBtns);
+        divBtns.append(editBtn, deleteBtn);
+    }
+
+
+    editBtn.addEventListener("click", () => {
+        isEditing = true;
+        input.value = h2.textContent;
+        addBtn.textContent = "Update";
+        taskId = li.dataset.tid;
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
     input.value = "";
 });
+
 
